33ed94d7dccb1ce6ee4647ed0334399043ec7708
Hayan Abou Reslan <hayyan.reslan@gmail.com>
2020-09-13 10:20:23 +0400

    Add Consumers.
    Add consumers table migration and consumer service provider

---
+++ database/migrations/2020_09_12_074847_create_consumers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConsumersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('consumers', function (Blueprint $table) {
+            $table->id();
+            $table->string('api_token', 60)->unique()->nullable()->default(null);
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('consumers');
+    }
+}
