84c6320b434a3377137ba2479e6fd1b3ba6896cb
Hayan Abou Reslan <hayyan.reslan@gmail.com>
2020-11-04 16:43:17 +0200

    Update customers migration

--- database/migrations/2020_09_06_100834_create_customers_table.php
+++ database/migrations/2020_09_06_100834_create_customers_table.php
@@ -16,6 +16,7 @@ class CreateCustomersTable extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('email')->unique();
             $table->text('description')->nullable();
             $table->boolean('is_active')->default(true);
             $table->timestamps();
