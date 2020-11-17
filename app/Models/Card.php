869504c9c161f723e88ef44b2cd57cf6b9fc7241
Hayan Abou Reslan <hayyan.reslan@gmail.com>
2020-11-17 12:01:26 +0200

    Add method to create card model from CardInterface

--- app/Models/Card.php
+++ app/Models/Card.php
@@ -4,10 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Payments\CardInterface;
+
 class Card extends Model
 {
     public function source()
     {
         return $this->morphOne(PaymentSource::class, 'sourceable');
+    }
+
+    //TODO::Refactor
+    public static function addCardFromPaymentResponse(Customer $customer, CardInterface $cardResponse)
+    {
+        $card = new self();
+        $card->customer_id = $customer->id;
+        $card->expiry_month = $cardResponse->getExpiryMonth();
+        $card->expiry_year = $cardResponse->getExpiryYear();
+        //TODO:: fix name to match the card's name
+        $card->name = $customer->name;
+        $card->scheme = $cardResponse->getScheme();
+        $card->last_four = $cardResponse->getLastFour();
+        $card->fingerprint = $cardResponse->getFingerPrint();
+        $card->bin = $cardResponse->getBIN();
+        $card->card_type = $cardResponse->getCardType();
+        $card->card_category = $cardResponse->getCardCategory();
+        $card->issuer = $cardResponse->getIssuer();
+        $card->country = $cardResponse->getIssuerCountry();
+        $card->product_id = $cardResponse->getProductId();
+        $card->product_type = $cardResponse->getProductType();
+        $card->avs_check = $cardResponse->getAVSCheck();
+        $card->cvv_check = $cardResponse->getCVVCheck();
+        $card->payouts = $cardResponse->getAllowsPayout();
+        $card->save();
+
+        return $card;
     }
 }
